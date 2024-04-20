75848672f947e831ffd6bad9c9bdf3aa945b5e6c
Zeinab Mahmoud <111311188+Zeinab-Mahmoud@users.noreply.github.com>
2024-04-20 13:03:23 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // 02
+    cout << "\n\nGuess the "
+    <<"Output\n";
+
+    cout << 10 + 20 - 5 << endl;
+    cout << "14 / 2" << "\n";
+    cout << 17 - 10 << "\n";
+    cout << "endl";
+    cout << "\npractice marks perfect" << endl;
+
+    // Homework 1#: Guess the run-time output :
+    /*
+
+
+        Guess the Output
+        25
+        14 / 2
+        7
+        endl
+        practice marks perfect
+    */
+    // Homework 2#: Write code to print the following
+
+    cout << "*\n";
+    cout << "* *\n";
+    cout << "* * *\n";
+    cout << "* * * *\n";
+    cout << "* * * * *\n";
+
+    cout <<"\n" << endl;
+
+    cout << "\n      *";
+    cout << "\n    * * *";
+    cout << "\n   * * * * *";
+    cout << "\n * * * * * * *";
+    cout << "\n* * * * * * * * *";
+
+    cout <<"\n" << endl;
+
+    cout << "  *\n";
+    cout << " ***\n";
+    cout << "*****\n";
+    cout << " ***\n";
+    cout << "  *\n";
+
+    cout <<"\n" << endl;
+
+    cout << "     1\n";
+    cout << "   2 3 2\n";
+    cout << "  3 4 5 4 3\n";
+    cout << " 4 5 6 7 6 5 4\n";
+    cout << "5 6 7 8 9 8 7 6 5\n";
+
+    cout <<"\n" << endl;
+
+    cout << "     1\n";
+    cout << "    1  1\n";
+    cout << "   1  2  1\n";
+    cout << "  1  3 3  1\n";
+    cout << " 1 4  6  4 1\n";
+    cout << "1 5  10  10 5 1\n";
+    return 0;
+}
